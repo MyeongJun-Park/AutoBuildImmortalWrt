3c490d15e033f841bebc4f97a36da9f7c0c39524
MyeongJun-Park <56251072+MyeongJun-Park@users.noreply.github.com>
2025-01-25 22:26:34 +0800

    更新 99-custom.sh

--- glinet/99-custom.sh
+++ glinet/99-custom.sh
@@ -16,9 +16,11 @@ else
    # 读取pppoe信息(由build.sh写入)
    . "$SETTINGS_FILE"
 fi
+
 # 无需判断网卡数量 因为glinet是多网口
 uci set network.lan.ipaddr='192.168.8.1'
 echo "set 192.168.8.1 at $(date)" >> $LOGFILE
+
 # 判断是否启用 PPPoE
 echo "print enable_pppoe value=== $enable_pppoe" >> $LOGFILE
 if [ "$enable_pppoe" = "yes" ]; then
@@ -45,5 +47,22 @@ uci commit
 FILE_PATH="/etc/openwrt_release"
 NEW_DESCRIPTION="Compiled by MyeongJun-Park"
 sed -i "s/DISTRIB_DESCRIPTION='[^']*'/DISTRIB_DESCRIPTION='$NEW_DESCRIPTION'/" "$FILE_PATH"
+
+# 创建新的网络接口 "USB"
+uci set network.usb='interface'                # 新建接口名称为 USB
+uci set network.usb.proto='dhcp'               # 设置协议为 DHCP 客户端
+uci set network.usb.device='eth2'              # 设置设备为 eth2
+
+# 分配防火墙区域为 WAN 和 WAN6
+uci add_list firewall.@zone[0].network='usb'   # 将 USB 接口添加到 WAN 区域
+uci add_list firewall.@zone[1].network='usb'   # 将 USB 接口添加到 WAN6 区域
+
+# 保存并应用配置
+uci commit network
+uci commit firewall
+
+# 重启相关服务
+/etc/init.d/network restart
+/etc/init.d/firewall restart
 
 exit 0
