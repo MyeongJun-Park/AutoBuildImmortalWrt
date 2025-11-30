144bdd2a41973daab40bb53c16d02755b5e1f637
MyeongJun-Park <56251072+MyeongJun-Park@users.noreply.github.com>
2025-11-30 23:03:26 +0800

    Update build23.sh

--- rockchip/build23.sh
+++ rockchip/build23.sh
@@ -62,7 +62,6 @@ PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
 PACKAGES="$PACKAGES openssh-sftp-server"
 PACKAGES="$PACKAGES luci-app-ramfree"
-PACKAGES="$PACKAGES luci-app-appfilter"
 # 代理工具
 PACKAGES="$PACKAGES luci-i18n-passwall-zh-cn"
 PACKAGES="$PACKAGES luci-app-openclash"
